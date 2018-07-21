6b8f96b009cac993fe156a273bd9b7f2ec29b8a1
Nkonda <nkonda@roamdata.com>
2018-07-21 18:24:15 -0400

    Added transition

--- app/src/main/java/com/udacity/nkonda/shopin/login/SplashScreenActivity.java
+++ app/src/main/java/com/udacity/nkonda/shopin/login/SplashScreenActivity.java
@@ -1,16 +1,11 @@
 package com.udacity.nkonda.shopin.login;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.widget.ImageView;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.udacity.nkonda.shopin.R;
 import com.udacity.nkonda.shopin.base.BaseActivity;
@@ -20,27 +15,53 @@ import com.udacity.nkonda.shopin.storelist.StoreListActivity;
 public class SplashScreenActivity extends BaseActivity {
     private static final String TAG = SplashScreenActivity.class.getSimpleName();
 
+    ImageView ivLogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ivLogo = findViewById(R.id.iv_logo);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent();
-        if (isOnline()) {
-            FirebaseUser currentUser = mAuth.getCurrentUser();
-            if (currentUser == null) {
-                intent.setClass(this, LoginActivity.class);
-            } else {
-                intent.setClass(this, StoreListActivity.class);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent();
+//                intent.setClass(SplashScreenActivity.this, LoginActivity.class);
+//                ActivityOptionsCompat options = ActivityOptionsCompat.
+//                        makeSceneTransitionAnimation(SplashScreenActivity.this,
+//                                ivLogo,
+//                                ivLogo.getTransitionName());
+//                startActivity(intent, options.toBundle());
+//            }
+//        }, 2000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                boolean currentUserLoggedIn = false;
+                if (isOnline()) {
+                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                    currentUserLoggedIn = currentUser != null;
+                }
+                if (currentUserLoggedIn) {
+                    intent.setClass(SplashScreenActivity.this, StoreListActivity.class);
+                    startActivity(intent);
+                } else {
+                    intent.setClass(SplashScreenActivity.this, LoginActivity.class);
+                    ActivityOptionsCompat options = ActivityOptionsCompat.
+                            makeSceneTransitionAnimation(SplashScreenActivity.this,
+                                    ivLogo,
+                                    ivLogo.getTransitionName());
+                    startActivity(intent, options.toBundle());
+                }
             }
-        } else {
-            intent.setClass(this, LoginActivity.class);
-        }
-//        intent.setClass(this, LoginActivity.class);
-        startActivity(intent);
+        }, 500);
+
+
     }
 }
